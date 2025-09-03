8f8d877cd004eeecddbefab5b4105846e31f64d7
Mart <martkaal1997@gmail.com>
2025-09-03 12:50:01 +0200

    button copy update

--- resources/js/pages/share.tsx
+++ resources/js/pages/share.tsx
@@ -32,8 +32,17 @@ export default function Share() {
     };
 
     const handleCopy = () => {
-        if (url) {
-            navigator.clipboard.writeText(url);
+        switch (mode) {
+            case 'generated':
+                if (url) {
+                    navigator.clipboard.writeText(url);
+                }
+                break;
+            case 'opened':
+                if (receivedPassword) {
+                    navigator.clipboard.writeText(receivedPassword);
+                }
+                break;
         }
     };
 
@@ -94,7 +103,7 @@ export default function Share() {
 
                     <button
                         type="button"
-                        className="me-2 mb-2 w-1/3 rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:cursor-pointer hover:bg-violet-700 focus:ring-1 focus:outline-none"
+                        className="me-2 mb-2 rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:cursor-pointer hover:bg-violet-700 focus:ring-1 focus:outline-none"
                         onClick={handleSubmit}
                     >
                         Creëer veilige link
@@ -112,7 +121,7 @@ export default function Share() {
                     />
                     <button
                         type="button"
-                        className="w-1/3 rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-violet-700 focus:ring-1"
+                        className="rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:cursor-pointer hover:bg-violet-700 focus:ring-1"
                         onClick={handleCopy}
                     >
                         Kopieer naar klembord
@@ -130,7 +139,7 @@ export default function Share() {
                     />
                     <button
                         type="button"
-                        className="w-1/3 rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:bg-violet-700 focus:ring-1"
+                        className="rounded-lg bg-violet-600 px-5 py-2.5 text-sm font-medium text-white hover:cursor-pointer hover:bg-violet-700 focus:ring-1"
                         onClick={handleCopy}
                     >
                         Kopieer naar klembord
